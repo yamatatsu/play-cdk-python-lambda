2a6cd7a77bd05cad1baf6695ab00e1d044aa365f
yamatatsu <the.kinnikuman@gmail.com>
2022-09-06 19:38:10 +0900

    Initial commit

---
+++ test/play-cdk-python-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PlayCdkPythonLambda from '../lib/play-cdk-python-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/play-cdk-python-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PlayCdkPythonLambda.PlayCdkPythonLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
